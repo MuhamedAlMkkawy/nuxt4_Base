5e2d89955e8f782a11163962b156a6ad9601f480
Muhmd AlMkkawy <89239134+MuhamedAlMkkawy@users.noreply.github.com>
2026-02-16 16:16:07 +0200

    Enhance the Nuxt Files

--- app/components/Empty.vue
+++ app/components/Empty.vue
@@ -1,33 +1,39 @@
 <template>
   <div class="empty">
     <NotFoundEyes />
-    <h3 class="mt-5">{{ emptyText }}</h3>
+    <h3 class="mt-5">{{ $t('empty_component.text') }}</h3>
   </div>
 </template>
 
-<script setup>
-  const globalStore = useGlobalStore();
-
-  const emptyText = ref(getEmptyText(globalStore.lang));
-
-  // Watch for changes in globalStore.lang and update emptyText accordingly
-  watch(
-    () => globalStore.lang,
-    (newLang) => {
-      emptyText.value = getEmptyText(newLang);
-    }
-  );
-
-  // Helper function to get the correct empty text based on the language
-  function getEmptyText(lang) {
-    return lang === 'ar' 
-      ? 'لايوجد محتوي حتي الأن' 
-      : lang === 'en' 
-      ? 'No Content Found Yet' : ''
-      // : 'ابھی تک کوئی مواد نہیں ہے۔';
-  }
-</script>
-
 <style lang="scss" scoped>
+.empty {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 40px 0;
+  min-height: 50vh;
+  i {
+    font-size: 150px;
+    margin-bottom: 20px;
+    color: $mainColor;
+    animation-name: rotate;
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    animation-direction: linear;
+  }
+  h3 {
+    font-size: 18px;
+    font-weight: 500;
+  }
+}
 
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
