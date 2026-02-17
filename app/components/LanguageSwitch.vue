ac4b305a637a8cbaeb21b16b08b5844e99f0e392
Muhmd AlMkkawy <89239134+MuhamedAlMkkawy@users.noreply.github.com>
2026-02-17 09:48:59 +0200

    Enhace the Language Switch

--- app/components/LanguageSwitch.vue
+++ app/components/LanguageSwitch.vue
@@ -1,28 +1,25 @@
 <template>
-  <div
-    class="languages-switch"
-    :style="'direction: ' + locale.code === 'ar' ? 'rtl' : 'ltr'"
+  <NuxtLink
+    class="language_switch"
+    :style="'direction: ' + currentDir"
+    v-for="locale in availableLocales"
+    :key="locale.code"
+    :to="switchLocalePath(locale.code)"
+    @click="useGlobalStore().setLang(locale.code)"
   >
-    <NuxtLink
-      v-for="locale in availableLocales"
-      :key="locale.code"
-      :to="switchLocalePath(locale.code)"
-      @click="useGlobalStore().setLang(locale.code)"
-    >
-      <!-- <span>{{ locale.name }}</span> -->
-      <span>{{ locale.code == "ar" ? "ع" : "En" }}</span>
-      <i class="pi pi-globe"></i>
-    </NuxtLink>
-  </div>
+    <!-- <span>{{ locale.name }}</span> -->
+    <span>{{ locale.code == "ar" ? "ع" : "En" }}</span>
+    <i class="pi pi-globe"></i>
+  </NuxtLink>
 </template>
 
 <script setup>
 const { locale, locales } = useI18n();
 const switchLocalePath = useSwitchLocalePath();
 
+const currentDir = computed(() => (locale.value == "ar" ? "rtl" : "ltr"));
+
 const availableLocales = computed(() => {
   return locales.value.filter((i) => i.code != locale.value);
 });
 </script>
-
-<style lang="scss" scoped></style>
