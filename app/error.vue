85b846339a6730aebddf871ff033589afb2d25c5
Muhmd AlMkkawy <89239134+MuhamedAlMkkawy@users.noreply.github.com>
2026-02-17 10:28:31 +0200

    Enhancing....

--- app/error.vue
+++ app/error.vue
@@ -1,71 +1,64 @@
 <template>
   <div class="error_page">
     <div class="image">
-      <img src="/assets/images/logo.png" alt="logo image" loading="lazy">
+      <img src="/assets/images/logo.svg" alt="logo image" loading="lazy" />
     </div>
+
     <h1>
-      4 
-      <NotFoundEyes/>
+      4
+      <NotFoundEyes />
       4
     </h1>
+
     <h3>
-      {{ 
-        globalStore.lang == 'en'?
-        'Page is Not Found ' :
-        'الصفحة غير موجوده'
-      }}
+      {{ $t("error_page.title") }}
     </h3>
+
     <NuxtLink :to="localeRoute('/')" class="main-btn">
-      {{ 
-        globalStore.lang == 'en'?
-        'Go To Home Page' :
-        'أذهب للرئيسية'
-      }}
+      {{ $t("error_page.go_home") }}
     </NuxtLink>
   </div>
 </template>
 
 <script setup>
-  import NotFoundEyes from './components/NotFoundEyes.vue';
-  
-  // define global store
-  const globalStore = useGlobalStore()
-
-  // define locale route
-  const localeRoute = useLocaleRoute()
+// define locale route
+const localeRoute = useLocaleRoute();
 </script>
 
-
 <style lang="scss">
-  .error_page{
-    @include displayFlex($direction : column , $gap : 0);
-    height: 100vh;
-    background: #fff;
-    position: relative;
-    .image{
-      width: 300px;
-      margin: 0 auto;
-      @media (max-width : 560px){
-        width: 200px;
-      }
+.error_page {
+  @include displayFlex($direction: column, $gap: 0);
+  height: 100vh;
+  background: $secColor;
+  position: relative;
+  .image {
+    width: 300px;
+    height: 200px;
+    margin: 0 auto;
+    img {
+      object-fit: contain;
     }
-
-    h1{
-      font-size: 150px;
-      margin-bottom: 0px;
-      color: $mainColor;
-      @include displayFlex($gap : 20px);
-      span{
-        color: $secColor;
-      }
-    }
-
-    h3{
-      font-size: clamp(30px , 100% , 300px);
-      margin-bottom: 10px;
-    }
-    a{
-      max-width: 500px;
+    @media (max-width: 560px) {
+      width: 200px;
     }
   }
+
+  h1 {
+    font-size: 150px;
+    margin-bottom: 0px;
+    color: $mainColor;
+    @include displayFlex($gap: 20px);
+    span {
+      color: $secColor;
+    }
+  }
+
+  h3 {
+    font-size: clamp(30px, 100%, 300px);
+    margin-bottom: 10px;
+  }
+  a {
+    max-width: 500px;
+  }
+}
 </style>
